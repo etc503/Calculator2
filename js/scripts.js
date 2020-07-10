0ceba3cf987052d712b4ea0fd528e93dbe560b62
Erin Cry <erin.cry@email.com>
2020-07-09 18:12:18 -0700

    add if else statements

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,11 @@ const divide = function(number1, number2) {
   return number1 / number2;
 };
 
+$(document).ready(function() {
+  const number1 = parseInt(prompt("Enter a number:"));
+  const number2 = parseInt(prompt("Enter another number:"));
 
+alert(add(number1, number2));
 });
 
 $(document).ready(function() {
@@ -23,16 +27,16 @@ $(document).ready(function() {
     const number1 = parseInt($("#input1").val());
     const number2 = parseInt($("#input2").val());
     const operator = $("input:radio[name=operator]:checked").val();
-    console.log("1st number: " + number1);  // for debugging
-    console.log("2nd number: " + number2);  // for debugging
-    console.log("operator: " + operator);  // for debugging
-    const result = add(number1, number2);
+    let result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
     $("#output").text(result);
   });
-
-  $(document).ready(function() {
-    const number1 = parseInt(prompt("Enter a number:"));
-    const number2 = parseInt(prompt("Enter another number:"));
-  
-  alert(add(number1, number2));
 });
